fa828b7dd814c88ab4c9b1b255ebf935fd3a47a1
Wil Taylor <cert@wiltaylor.dev>
2021-12-02 20:03:43 +1000

    Day 1 part2

--- 2021/go/day1/main.go
+++ 2021/go/day1/main.go
@@ -8,9 +8,24 @@ import (
   "strconv"
 )
 
-func part1(){
-  f, err := os.Open("../../input/day1.1")
+func windowBuilder(lst []int) []int {
+  result := make([]int, 0)
 
+  for index := range lst {
+    if len(lst) - index < 3 {
+      break
+    }
+
+    result = append(result, lst[index] + lst[index + 1] + lst[index + 2])
+  }
+
+  return result
+}
+
+func getListOfNumbers() []int {
+  result := make([]int, 0)
+
+  f, err := os.Open("../../input/day1.1")
   if err != nil {
     log.Fatal(err)
   }
@@ -19,35 +34,44 @@ func part1(){
   
   scanner := bufio.NewScanner(f)
 
+  for scanner.Scan() {
+    current, _ := strconv.Atoi(scanner.Text())
+    result = append(result, current)
+  }
+
+  return result
+}
+
+func countIncreaseInList(lst []int) int {
   last := 0
   first := true
   count := 0;
 
-  for scanner.Scan() {
-    current, _ := strconv.Atoi(scanner.Text())
-    text := "unknown"
-
+  for _, val := range lst {
     if !first {
-      if last < current {
+      if last < val {
         count++
-        text = "increase"
-      }else if last == current {
-        text = "same"
-      }else{
-        text = "decrease"
       }
     }else{
-      first = false
+      first = false;
     }
 
-    fmt.Printf("%d -> %d - %s\n",last,current, text)
-
-    last = current
+    last = val
   }
 
-  fmt.Printf("Total Count: %d\n", count)
+  return count
+}
+
+func part1() int{
+  return countIncreaseInList(getListOfNumbers())
+}
+
+func part2() int{
+  lst := windowBuilder(getListOfNumbers())
+  return countIncreaseInList(lst)
 }
 
 func main(){
-  part1()
+  fmt.Printf("Part 1: %d\n", part1())
+  fmt.Printf("Part 2: %d\n", part2())
 }
--- 2021/nix/day1.nix
+++ 2021/nix/day1.nix
@@ -4,7 +4,18 @@ let
   input1 = readFile ../input/day1.1;
   splitLines = text: filter (item: (isString item && item != "")) (split "\n" text);
   data1 = map (f: toInt f) (splitLines input1);
+  data2 = buildWindow data1;
   toInt = str: fromJSON str; # Very hacky but is actually how nixpkgs does it too.
+
+  buildWindow = lst: let
+    canNext = if canWork then (length (tail lst) >= 3) else false;
+    canWork = if (isList lst && (length lst >= 3)) then true else false;
+    a = head lst;
+    b = head (tail lst);
+    c = head (tail (tail lst));
+    result = if canWork then ([(a + b + c)]) else [];
+    extra = if canWork then (if canNext then (buildWindow (tail lst)) else []) else [];
+  in result ++ extra;
 
   countIncreases = {last ? null, lst}: let
     current = head lst;
@@ -14,5 +25,5 @@ let
   in result + next; 
 in {
   part1 = ''${toString (countIncreases {lst = data1;})}'';
-  part2 = ''test2'';
+  part2 = ''${toString (countIncreases {lst = data2;})}'';
 }
