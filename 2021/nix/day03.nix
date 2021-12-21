982428e3c97736aab64c826218bba5eff4cdb8d8
Wil Taylor <cert@wiltaylor.dev>
2021-12-21 20:51:19 +1000

    Completed day 3 in nix

--- 2021/nix/day03.nix
+++ 2021/nix/day03.nix
@@ -5,6 +5,35 @@ let
   splitLines = text: filter (item: (isString item && item != "")) (split "\n" text);
   toInt = str: fromJSON str; # Very hacky but is actually how nixpkgs does it too.
 
+  str2bin = txt: let
+    value = genList (x: if ((substring x 1 txt) == "1") then 1 else 0) (stringLength txt);
+  in value;
+
+  #Looks for most common bit. If they are both as common returns 1.
+  findMostCommonBit = idx: lst: let
+    counts = foldl' (left: right: let
+      v = elemAt right idx;
+    in {
+      ones = if (v == 1) then (left.ones + 1) else left.ones;
+      zeros = if (v == 0) then (left.zeros + 1) else left.zeros;
+    }) { ones = 0; zeros = 0; } lst;
+
+    result = if (counts.zeros > counts.ones) then 0 else 1;
+  in result;
+
+  findLeastCommonBit = idx: lst: let
+    counts = foldl' (left: right: let
+      v = elemAt right idx;
+    in {
+      ones = if (v == 1) then (left.ones + 1) else left.ones;
+      zeros = if (v == 0) then (left.zeros + 1) else left.zeros;
+    }) { ones = 0; zeros = 0; } lst;
+
+    result = if (counts.ones >= counts.zeros) then 0 else 1;
+  in result;
+
+  filterListBasedOnBit = idx: bitValue: lst: filter (v: (elemAt v idx) == bitValue) lst;
+
   bin2dec = lst: let
     calc = idx: factor: let
       e = elemAt lst idx;
@@ -38,6 +67,11 @@ let
     values = genList (idx: countPos lineData idx) (stringLength firstLine);
   in values;
 
+  allValuesBinary = let 
+    lineData = splitLines input1;
+    values = map(line: str2bin line) lineData;
+  in values;
+
   gammaBin = let
     lst = genList (idx: elemAt allData idx) (length allData);
     values = map (x: if(x.one > x.zero) then 1 else 0) lst;
@@ -48,16 +82,40 @@ let
     values = map (x: if(x.one < x.zero) then 1 else 0) lst;
   in values;
 
+  oxygenGenerationBin = let
+    calc = idx: lst: let
+      bitVal = findMostCommonBit idx lst;
+      filterLst = filterListBasedOnBit idx bitVal lst;
+
+      val = if (length filterLst == 1) then (head filterLst) else (calc (idx + 1) filterLst);
+    in val;
+
+    values = calc 0 allValuesBinary;
+  in values;
+
+  co2ScrubberBin = let 
+    calc = idx: lst: let
+      bitValue = findLeastCommonBit idx lst;
+      filterLst = filterListBasedOnBit idx bitValue lst;
+
+      val = if (length filterLst == 1) then (head filterLst) else (calc (idx + 1) filterLst);
+    in val;
+
+    values = calc 0 allValuesBinary;
+  in values;
+
   gamma = bin2dec gammaBin;
   epsilon = bin2dec epsilonBin;
+  oxygenGeneration = bin2dec oxygenGenerationBin;
+  co2Scrubber = bin2dec co2ScrubberBin;
   
   data1 = gamma * epsilon;
-  data2 = 0;
-
-
+  data2 = oxygenGeneration * co2Scrubber;
 in ''
   Gamma: ${toJSON gammaBin} -> ${toString gamma}
   epsilon: ${toJSON epsilonBin} -> ${toString epsilon}
+  oxygenGeneration: ${toJSON oxygenGenerationBin} -> ${toString oxygenGeneration}
+  co2Scrubber: ${toJSON co2ScrubberBin} -> ${toString co2Scrubber}
 
   Part 1: ${toString data1}
   Part 2: ${toString data2}
